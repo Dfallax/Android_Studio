98056c8a5090730af2a6efa3a0ffe02152d00dbe
Dfallax <dfalla0001@gmail.com>
2023-10-18 12:56:09 +0200

    Update_Initial

--- app/src/main/java/com/example/test/Show.java
+++ app/src/main/java/com/example/test/Show.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.test.Database.DatabaseAux;
@@ -30,17 +31,23 @@ public class Show extends AppCompatActivity {
         SQLiteDatabase db = new DatabaseAux(this).getReadableDatabase();
 
         Cursor cursor = db.rawQuery("SELECT * FROM users", null);
-        TextView showNameView = findViewById(R.id.showName);
-        TextView showEmailView = findViewById(R.id.showEmail);
+        //TextView showNameView = findViewById(R.id.showName);
+        //TextView showEmailView = findViewById(R.id.showEmail);
+
+        LinearLayout layout = findViewById(R.id.ContentShow);
+
 
         if(cursor.moveToFirst()) {
             do {
-                int id = cursor.getInt(0);
+                int id = cursor.getInt(0);//sacamos las posiciones de la base de datos
                 String name = cursor.getString(1);
                 String email = cursor.getString(2);
 
-                showNameView.setText(id + " " + name);
-                showEmailView.setText(email);
+                TextView data = new TextView(this);
+                data.setText("Nombre: "+ name +" Email: "+ email);//ingreamos datos al objeto data
+                layout.addView(data);// acá le añadimos al layou para que se muestre los resultados
+                //showNameView.setText(id + " " + name);
+               // showEmailView.setText(email);
             }while(cursor.moveToNext());
         }
 
